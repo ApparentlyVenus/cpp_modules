1bd56af9671db214f7e4171f4b0a56be68d6cee7
Omar Dana <odana@c1r7s1.42beirut.com>
2025-08-23 19:04:44 +0300

    updated makefile, main function for complation error, and tweaks in intern.cpp

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: odana <odana@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 18:43:32 by odana             #+#    #+#             */
-/*   Updated: 2025/08/23 18:46:42 by odana            ###   ########.fr       */
+/*   Updated: 2025/08/23 19:03:44 by odana            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 Intern::Intern()
 {
-    std::cout << "Intern default constructor called" << std::endl;
-    
     formCreators[0] = &Intern::createShrubberyCreationForm;
     formCreators[1] = &Intern::createRobotomyRequestForm;
     formCreators[2] = &Intern::createPresidentialPardonForm;
@@ -27,7 +25,6 @@ Intern::Intern()
 
 Intern::Intern(const Intern& other)
 {
-    std::cout << "Intern copy constructor called" << std::endl;
     for (int i = 0; i < 3; i++)
     {
         formCreators[i] = other.formCreators[i];
@@ -37,7 +34,6 @@ Intern::Intern(const Intern& other)
 
 Intern& Intern::operator=(const Intern& other)
 {
-    std::cout << "Intern copy assignment operator called" << std::endl;
     if (this != &other)
     {
         for (int i = 0; i < 3; i++)
@@ -49,24 +45,21 @@ Intern& Intern::operator=(const Intern& other)
     return *this;
 }
 
-Intern::~Intern()
-{
-    std::cout << "Intern destructor called" << std::endl;
-}
+Intern::~Intern() {}
 
 AForm* Intern::createShrubberyCreationForm(const std::string& target) const
 {
-    return new ShrubberyCreationForm(target);
+    return (new ShrubberyCreationForm(target));
 }
 
 AForm* Intern::createRobotomyRequestForm(const std::string& target) const
 {
-    return new RobotomyRequestForm(target);
+    return (new RobotomyRequestForm(target));
 }
 
 AForm* Intern::createPresidentialPardonForm(const std::string& target) const
 {
-    return new PresidentialPardonForm(target);
+    return (new PresidentialPardonForm(target));
 }
 
 AForm* Intern::makeForm(const std::string& formName, const std::string& target) const
@@ -76,9 +69,9 @@ AForm* Intern::makeForm(const std::string& formName, const std::string& target) 
         if (formNames[i] == formName)
         {
             std::cout << "Intern creates " << formName << std::endl;
-            return (this->*formCreators[i])(target);
+            return ((this->*formCreators[i])(target));
         }
     }
     std::cout << "Error: Form name \"" << formName << "\" does not exist" << std::endl;
-    return NULL;
+    return (NULL);
 }
--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: odana <odana@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 03:21:47 by odana             #+#    #+#             */
-/*   Updated: 2025/08/23 18:50:28 by odana            ###   ########.fr       */
+/*   Updated: 2025/08/23 19:02:52 by odana            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,33 @@ int main()
 
     AForm* form4 = intern.makeForm("coffee making", "kitchen");
 
-    if (form1) {
+    if (form1)
+    {
         boss.signForm(*form1);
         boss.executeForm(*form1);
         delete form1;
     }
 
-    if (form2) {
+    if (form2)
+    {
         boss.signForm(*form2);
         boss.executeForm(*form2);
         delete form2;
     }
 
-    if (form3) {
+    if (form3)
+    {
         boss.signForm(*form3);
         boss.executeForm(*form3);
         delete form3;
     }
 
+    if (form4)
+    {
+        boss.signForm(*form4);
+        boss.executeForm(*form4);
+        delete form4;
+    }
+    
     return (0);
 }
