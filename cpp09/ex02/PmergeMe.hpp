dce8d7f651cdfa459b018b434c2dc2cba02645d3
Omar Dana <odana@c1r8s2.42beirut.com>
2025-09-01 14:27:03 +0300

    preliminary ex02 setup

---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odana <odana@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/01 14:22:26 by odana             #+#    #+#             */
+/*   Updated: 2025/09/01 14:26:40 by odana            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+# include <iostream>
+# include <vector>
+# include <deque>
+# include <string>
+# include <sstream>
+# include <exception>
+# include <ctime>
+# include <sys/time.h>
+
+class PmergeMe
+{
+public:
+    PmergeMe();
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& other);
+    ~PmergeMe();
+    
+    void processArguments(int argc, char** argv);
+    void displayResults();
+    
+private:
+    std::vector<int>    _inputNumbers;
+    std::vector<int>    _sortedVector;
+    std::deque<int>     _sortedDeque;
+    
+    double _vectorTime;
+    double _dequeTime;
+    
+    // Input Processing 
+    void parseAndValidateInput(int argc, char** argv);
+    bool isValidPositiveInteger(const std::string& str);
+    void checkForDuplicates();
+    
+    // Jacobsthal & Helper Functions
+    std::vector<int> generateJacobsthalSequence(int maxSize);
+    std::vector<int> createInsertionOrder(int pendSize);
+    double getCurrentTime();
+    
+    // Display 
+    void printSequence(const std::vector<int>& sequence, const std::string& label);
+    void printTimingResults();
+    
+    // FORD-JOHNSON ALGORITHM - VECTOR IMPLEMENTATION
+    void sortWithVector();
+    std::vector<int> fordJohnsonVector(std::vector<int>& data);
+    
+    void createPairsVector(const std::vector<int>& data, 
+                          std::vector<int>& mainChain, 
+                          std::vector<int>& pendChain, 
+                          int& extraElement, 
+                          bool& hasExtra);
+    
+    std::vector<int> recursiveSortVector(std::vector<int>& mainChain);
+    
+    void insertPendElementsVector(std::vector<int>& mainChain, 
+                                 const std::vector<int>& pendChain, 
+                                 int extraElement, 
+                                 bool hasExtra);
+    
+    int binarySearchVector(const std::vector<int>& container, 
+                          int value, 
+                          int left, 
+                          int right);
+    
+    void insertAtPositionVector(std::vector<int>& container, 
+                               int value, 
+                               int position);
+    
+    // FORD-JOHNSON ALGORITHM - DEQUE IMPLEMENTATION
+    void sortWithDeque();
+    std::deque<int> fordJohnsonDeque(std::deque<int>& data);
+    
+    void createPairsDeque(const std::deque<int>& data, 
+                         std::deque<int>& mainChain, 
+                         std::deque<int>& pendChain, 
+                         int& extraElement, 
+                         bool& hasExtra);
+    
+    std::deque<int> recursiveSortDeque(std::deque<int>& mainChain);
+    
+    void insertPendElementsDeque(std::deque<int>& mainChain, 
+                                const std::deque<int>& pendChain, 
+                                int extraElement, 
+                                bool hasExtra);
+
+    int binarySearchDeque(const std::deque<int>& container, 
+                         int value, 
+                         int left, 
+                         int right);
+    
+    void insertAtPositionDeque(std::deque<int>& container, 
+                              int value, 
+                              int position);
+    
+};
+
+#endif
